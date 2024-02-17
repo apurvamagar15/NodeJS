cd19a071ab91d0e377118a604ff65001131e93eb
apurvamagar15 <159618512+apurvamagar15@users.noreply.github.com>
2024-02-17 15:15:15 +0530

    Add files via upload

--- demofile.js
+++ demofile.js
@@ -1,9 +1,9 @@
-var http=require('http');
-var fs=require('fs');
-http.createServer(function(req,res){
-    fs.readFile('text.html',function(err,data){
-        res.writeHead(200,{'content-type':'text/html'});
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req,res) {
+    fs.readFile('demo.html',function(err,data) {
+        res.writeHead(200,{'Content-type':'text/html'});
         res.write(data);
         return res.end();
-});
-}).listen(8081);
+    });
+}).listen(8082);
